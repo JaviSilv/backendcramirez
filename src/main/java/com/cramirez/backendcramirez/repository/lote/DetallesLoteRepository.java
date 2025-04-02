7bafa0ed2fe1aee0f47556bec06e67ac37f85f99
HP <luis.cisneros.salas03@gmail.com>
2025-04-02 13:25:36 -0500

    "delet detalles lote"

--- src/main/java/com/cramirez/backendcramirez/repository/lote/DetallesLoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cramirez.backendcramirez.repository.lote;
-
-import com.cramirez.backendcramirez.entity.lote.DetallesLote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetallesLoteRepository extends JpaRepository<DetallesLote, Integer> {
-}
